b072378f08003b6002525b9d12010609e8b9dc73
rinshafebin <rinshafebinkk12@gmail.com>
2025-12-08 08:32:28 +0530

    updated

--- stylenest_backend/urls.py
+++ stylenest_backend/urls.py
@@ -5,8 +5,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Stylenest API is running!"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/products/', include('products.urls')),
