f5d9e25a85991e4e2c09bc717711bc07eca6c114
rinshafebin <rinshafebinkk12@gmail.com>
2025-12-08 08:24:22 +0530

    updated

--- stylenest_backend/settings.py
+++ stylenest_backend/settings.py
@@ -158,3 +158,7 @@ RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 # AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
 # AWS_QUERYSTRING_AUTH = False
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
