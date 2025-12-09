37babde588fddf936620c9b806133d89cc91f51d
rinshafebin <rinshafebinkk12@gmail.com>
2025-12-09 06:34:41 +0530

    updated

--- products/serializers.py
+++ products/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-
-    category = serializers.ChoiceField(choices=Product.CATEGORY_CHOICES)
+    image = serializers.CharField(source="image_url", required=False)
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'slug', 'description', 'details', 'price',
+            'category', 'stock', 'image', 'is_active',
+            'created_at', 'updated_at'
+        ]
         read_only_fields = ['created_at', 'updated_at', 'slug']
 
-    # Validations
     def validate_name(self, value):
         if not value or len(value.strip()) < 3:
             raise serializers.ValidationError("Product name must have at least 3 characters.")
@@ -26,7 +28,7 @@ class ProductSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Stock cannot be negative.")
         return value
 
-    def validate_image_url(self, value):
+    def validate_image(self, value):
         if value and not value.startswith("http"):
-            raise serializers.ValidationError("Image URL must be a valid URL.")
+            raise serializers.ValidationError("Image must be a valid URL.")
         return value
