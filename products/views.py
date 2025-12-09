d239e9b00d6d7b15db7fdf5d1ab1b874b50cf2af
rinshafebin <rinshafebinkk12@gmail.com>
2025-12-09 07:55:50 +0530

    updated

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ from django.db import IntegrityError
 
 # ------------------------- Pagination -------------------------
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 100
 
